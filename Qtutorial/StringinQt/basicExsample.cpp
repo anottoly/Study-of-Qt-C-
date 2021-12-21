3f15372b13a7b85d0670d70b2ef6e52a69bbd87c
Anatoliy Sinelnikov <tolyasiksin@gmail.com>
2021-12-21 07:09:37 +0400

    Add directory for Qt tutorial

---
+++ Qtutorial/StringinQt/basicExsample.cpp
@@ -0,0 +1,17 @@
+#include <QTextStream>
+
+int main(void)
+{
+    QTextStream cout(stdout);
+    
+    QString a {"goose"};
+    a.append (" is a baaaad guy")
+    a.prepebd ("Our ")
+    cout << a << endl;
+    cout << "Here're a " << a.count() << " symbols";
+    cout << a.toUpper() 
+    << endl;
+    cout << a.toLower()
+    << endl;
+    return 0;
+}
